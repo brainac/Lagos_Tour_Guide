72c91a4aad9651821eb12c06f1aba09c32567783
brainac <nedu4u2k4@gmail.com>
2021-10-06 20:04:54 +0100

    Lots of adjustment

---
+++ app/src/main/java/com/example/lagostourguide/AttractionAdapter.java
@@ -0,0 +1,64 @@
+package com.example.lagostourguide;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class AttractionAdapter extends FragmentPagerAdapter {
+
+    /** Context of the app */
+    private final Context mContext;
+
+    /**
+     * Create a new {@link AttractionAdapter} object.
+     *
+     * @param context is the context of the app
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
+    public AttractionAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new HotelsFragment();
+        } else if (position == 1) {
+            return new MallsFragment();
+        } else if (position == 2) {
+            return new MuseumsFragment();
+        } else {
+            return new BeachesFragment();
+        }
+    }
+
+    /**
+     * Return the total number of pages.
+     */
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.attraction_hotels);
+        } else if (position == 1) {
+            return mContext.getString(R.string.attraction_malls);
+        } else if (position == 2) {
+            return mContext.getString(R.string.attraction_museums);
+        } else {
+            return mContext.getString(R.string.attraction_beaches);
+        }
+    }
+}
